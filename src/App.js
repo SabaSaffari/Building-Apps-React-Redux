3ef98846d521f6e006fc8f9de10d80053628dd2b
saba <saba.sfrm@gmail.com>
2019-04-05 18:15:40 +1100

    add all files

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {Route} from "react-router-dom";
+import HomePage from "./home/HomePage";
+import AboutPage from "./about/AboutPage";
+
+
+function App() {
+  return(
+    <div className="container-fluid">
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/about" component={AboutPage} />
+
+    </div>
+
+  );
+}
+
+export default App;
